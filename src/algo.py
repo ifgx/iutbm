71cea99fa1037a79f25ebe78b0cdbf625d50f8e9
jvoisin <pouicpouicpouic@gmail.com>
2011-09-15 10:09:08 +0200

    Creation des fichiers de base

---
+++ src/algo.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+
+import pygame
+
+import solve
+import explain
+import play
+
+class algo:
+	'''
+		Classe abstraite representant un
+		algorithme
+	'''
+	def __init__():
+		pass
+
+	def play():
+	'''
+		Permet au joueur d'essayer de resoudre
+		le probleme pose
+	'''
+		pass
+
+	def solve():
+	'''
+		Resolution pas-a-pas du probleme
+	'''
+		pass
+	
+	def explain():
+	'''
+		Petit texte explicatif/historique au sujet
+		du probleme pose
+	'''
+		pass
