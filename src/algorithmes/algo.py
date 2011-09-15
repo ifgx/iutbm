66857e73342e66139770add93cb87206b8b6dea7
jvoisin <pouicpouicpouic@gmail.com>
2011-09-15 10:42:59 +0200

    Deplacement, et creation de voayge.py

---
+++ src/algorithmes/algo.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+import pygame
+
+class algo:
+	'''
+		Classe abstraite representant un
+		algorithme
+	'''
+	def __init__():
+		pass
+
+	def play():
+		'''
+			Permet au joueur d'essayer de resoudre
+			le probleme pose
+		'''
+		pass
+
+	def solve():
+		'''
+			Resolution pas-a-pas du probleme
+		'''
+		pass
+	
+	def explain():
+		'''
+			Petit texte explicatif/historique au sujet
+			du probleme pose
+		'''
+		pass
---
+++ src/algorithmes/voyage.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+
+class voyage(algo):
+	'''
+		Probleme du voyageur de commerce
+	'''
+	def __init__:
+		pass
