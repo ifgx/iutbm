7fdabd83c3f338c92928e909bbbd30f18521a68f
jvoisin <pouicpouicpouic@gmail.com>
2011-09-15 10:24:29 +0200

    Un peu plus d'organisation

--- src/iutbm.py
+++ src/iutbm.py
@@ -1,18 +1,33 @@
 #!/bin/env python
 
 import algo
+import pygame
+import sys
+
 
 class iutbm:
 	'''
 		Classe principale
 	'''
-	#TODO : Remplir un peu la fenetre
-	pass
+	def __init__():
+		self.w = window()
+
+	def main():
+		while True:
+			self.w.screen.fill(black)
+			pygame.display.flip()
 
 
 class window:
 	'''
 		Fenetre principale
 	'''
-	#TODO : Creer la fenetre
-	pass
+	def __init__(self):
+		pygame.init()
+		size = width, height = 800, 600
+		self.screen = pygame.display.set_mode(size)
+
+
+if __name__ == '__main__':
+	a = iutbm
+	a.main()
