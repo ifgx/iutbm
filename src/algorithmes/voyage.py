f0e1ba5203b21a58ef7a48e6add7f433ab6f0788
jvoisin <pouicpouicpouic@gmail.com>
2011-09-21 17:20:12 +0200

    resolution gloutonne du voyageur de commerce

--- src/algorithmes/voyage.py
+++ src/algorithmes/voyage.py
@@ -1,8 +1,19 @@
 #!/bin/env python
 
-class voyage(algo):
+import graphe
+
+class Voyage(algo):
 	'''
 		Probleme du voyageur de commerce
 	'''
-	def __init__:
-		pass
+	def __init__(self):
+		self.graphe = graphe.Graphe(10, 20, 1, 10)
+	
+	def _solve(self):
+		liste_liens = []  # liste ordonnee des liens a parcourir
+		sommet_actuel = self.debut
+		for i in self.liste_noeuds:
+			liste_lien.append(sommet_actuel)
+			sommet_actuel.visite = True
+			sommet_actuel = plus_proche_voisin(sommet_actuel)
+
