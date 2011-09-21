154583965911f4d9527563dae6c8466bac9a872e
jvoisin <pouicpouicpouic@gmail.com>
2011-09-21 15:52:28 +0200

    graphe.py est une classe implementant un graphe

---
+++ src/algorithmes/graphe.py
@@ -0,0 +1,59 @@
+import random
+
+
+class Graphe(object):
+	def __init__(self):
+		self.graphe = []
+	
+	def ajout_lien(self, lien):
+		if lien in self.graphe:
+			#suppression du doublon
+			self.graphe.pop(lien)
+		self.graphe.append(lien)
+
+	def supprime_lien(self, lien):
+		if lien in self.graphe:
+			self.graphe.pop(lien)
+		else:  # FIXME
+			raise
+	
+	def affiche(self):
+		'''
+			Affiche sur la sortie standard
+			le graphe (debug)
+		'''
+		for i in self.graphe:
+			print 'point1 : %s' % i.lien[0].nom
+			print 'point2 : %s' % i.lien[1].nom
+			print 'poids du lien : %s' % i.lien[2]
+			print '\n'
+		
+
+class Point(object):
+	def __init__(self, nom):
+		self.nom = nom
+		self.visite = False
+
+
+class Lien(object):
+	def __init__(self, sommet1, sommet2, poids):
+		self.nom = str(sommet1.nom) + " - " + str(sommet2.nom)
+		self.lien = (sommet1, sommet2, poids)
+
+
+def generation_graphe(nbNoeuds, nbLiens, minPoids, maxPoids):
+	'''
+		Retourne un graphe
+	'''
+	liste_noeuds = [Point(i) for i in xrange(nbNoeuds)]
+	graphe = Graphe()
+	for i in xrange(nbLiens):
+		noeud1 = random.choice(liste_noeuds)	
+		noeud2 = random.choice(liste_noeuds)	
+		poids = random.randint(minPoids, maxPoids)
+		lien = Lien(noeud1, noeud2, poids) 
+		graphe.ajout_lien(lien)
+	return graphe
+
+#bleh = generation_graphe(10, 11, 1, 14)
+#bleh.affiche()
