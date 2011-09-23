4a419dab4f0ef9eb75ff3fb9a7c215b45abdc16b
jvoisin <pouicpouicpouic@gmail.com>
2011-09-23 16:50:23 +0200

    Ameliorations

--- src/algorithmes/voyage.py
+++ src/algorithmes/voyage.py
@@ -1,8 +1,9 @@
 #!/bin/env python
 
 import graphe
+import algo
 
-class Voyage(algo):
+class Voyage(algo.algo):
 	'''
 		Probleme du voyageur de commerce
 	'''
@@ -10,10 +11,16 @@ class Voyage(algo):
 		self.graphe = graphe.Graphe(10, 20, 1, 10)
 	
 	def _solve(self):
-		liste_liens = []  # liste ordonnee des liens a parcourir
-		sommet_actuel = self.debut
-		for i in self.liste_noeuds:
-			liste_lien.append(sommet_actuel)
+		liste_sommets = []  # liste ordonnee des sommets a parcourir
+		sommet_actuel = self.graphe.debut
+		for i in self.graphe.liste_sommets:
+			liste_sommets.append(sommet_actuel)
 			sommet_actuel.visite = True
-			sommet_actuel = plus_proche_voisin(sommet_actuel)
+			sommet_actuel = self.graphe.plus_proche_voisin(sommet_actuel)
+		for i in liste_sommets:
+			i.affiche()
+
+b = Voyage()
+#b.graphe.affiche()
+b._solve()
 
