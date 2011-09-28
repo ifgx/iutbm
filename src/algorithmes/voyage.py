8b15b11ceada396480c3081be6e22573812c67ae
jvoisin <pouicpouicpouic@gmail.com>
2011-09-28 16:33:36 +0200

    POO

--- src/algorithmes/voyage.py
+++ src/algorithmes/voyage.py
@@ -7,6 +7,16 @@ import math
 import graphe
 import algo
 
+class Sommet:
+	def __init__(self, nom, x, y):
+		self.nom = nom
+		self.x = x
+		self.y = y
+		self.visite = False
+
+	def __repr__(self):
+		return str(self.nom)
+
 class Voyage(algo.algo):
 	'''
 		Probleme du voyageur de commerce
@@ -24,18 +34,18 @@ class Voyage(algo.algo):
 		for cpt in xrange(nbpoints):
 			x = random.randint(minx, maxx)
 			y = random.randint(miny, maxy)
-			matrix[0][cpt] = [cpt, x, y]
-			matrix[cpt][0] = [cpt, x, y]
+			matrix[0][cpt] = Sommet(cpt, x, y) #[cpt, x, y]
+			matrix[cpt][0] = Sommet(cpt, x, y) #[cpt, x, y]
 		matrix[0][0] = ''
 
 
 		for i in xrange(nbpoints - 1):
 			for j in xrange(nbpoints - 1):
 				if i != j:
-					x1 = matrix[0][i+1][1]
-					y1 = matrix[0][i+1][2]
-					x2 = matrix[j+1][0][1]
-					y2 = matrix[j+1][0][2]
+					x1 = matrix[0][i+1].x#[1]
+					y1 = matrix[0][i+1].y#[2]
+					x2 = matrix[j+1][0].x#[1]
+					y2 = matrix[j+1][0].y#[2]
 					distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 					matrix[i+1][j+1] = int(distance) #debug
 
