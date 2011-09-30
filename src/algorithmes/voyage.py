d156fd72839862259d81c219817cc7f6338c71ed
jvoisin <pouicpouicpouic@gmail.com>
2011-09-30 16:31:13 +0200

    Poo

--- src/algorithmes/voyage.py
+++ src/algorithmes/voyage.py
@@ -27,30 +27,44 @@ class Voyage(algo.algo):
 		maxx = 10
 		miny = 0
 		maxy = 10
+		self.matrix = self._create_matrix(nbpoints, minx, maxx, miny, maxy)
+		for i in self.matrix:
+			print i
 
-		#matrix = [['0']*nbpoints]*nbpoints  # FIXME
+
+	def _create_matrix(self, nbpoints, minx, maxx, miny, maxy):
+		'''
+			retourne un matrice de la forme:
+				T 1 2 3 4 ... nbpoints
+				1 0 d d d ...
+				2 d 0 d d
+				3 d d 0 d
+				4 d d d 0
+			
+			avec:
+				T = nombre de lignes
+				d = distance entre les deux points
+					correspondants
+		'''
 		matrix = [[0 for i in xrange(nbpoints)] for j in xrange(nbpoints)]
 
 		for cpt in xrange(nbpoints):
 			x = random.randint(minx, maxx)
 			y = random.randint(miny, maxy)
-			matrix[0][cpt] = Sommet(cpt, x, y) #[cpt, x, y]
-			matrix[cpt][0] = Sommet(cpt, x, y) #[cpt, x, y]
-		matrix[0][0] = ''
-
+			matrix[0][cpt] = Sommet(cpt, x, y) 
+			matrix[cpt][0] = Sommet(cpt, x, y) 
+		matrix[0][0] = nbpoints
 
 		for i in xrange(nbpoints - 1):
 			for j in xrange(nbpoints - 1):
 				if i != j:
-					x1 = matrix[0][i+1].x#[1]
-					y1 = matrix[0][i+1].y#[2]
-					x2 = matrix[j+1][0].x#[1]
-					y2 = matrix[j+1][0].y#[2]
+					x1 = matrix[0][i+1].x
+					y1 = matrix[0][i+1].y
+					x2 = matrix[j+1][0].x
+					y2 = matrix[j+1][0].y
 					distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 					matrix[i+1][j+1] = int(distance) #debug
 
-		for i in matrix:
-			print i
+		return matrix
 
 b = Voyage()
-
