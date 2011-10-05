1e3dce2889ae6b263062ea838e37b478d9a2383e
czambel2 <zmbl.soft@gmail.com>
2011-10-05 17:03:14 +0200

    Avancement de l'algorithme

--- src/algorithmes/couplage.py
+++ src/algorithmes/couplage.py
@@ -9,12 +9,8 @@ class ModelisationCouplage:
     """
     def element_en_liste(self, liste, element, premier):
         for elemt in liste:
-            if premier:
-                if elemt[0] == element:
-                    return True
-            else:
-                if elemt[1] == element:
-                    return True
+            if elemt[1 - premier] == element:
+                return True
         return False
 
     def __init__(self, listeFourmis, listeDesserts, nbCouples):
@@ -26,65 +22,91 @@ class ModelisationCouplage:
         
         assert nbCouples <= (len(listeFourmis) * len(listeDesserts))
         
-        for i in range(nbCouples):
-            is_init = True
-            while(is_init or self.preferences.count((fourmi, dessert)) > 0):
-                is_init = False
+        for i in xrange(nbCouples):
+            fourmi = choice(listeFourmis)
+            dessert = choice(listeDesserts)
+            while self.preferences.count((fourmi, dessert)):
                 fourmi = choice(listeFourmis)
                 dessert = choice(listeDesserts)
-
             self.preferences.append((fourmi, dessert))
         
         # On ajoute un couple pour chaque fourmi et chaque dessert qui ne sont pas encore satisfaites/servis
         for fourmi in listeFourmis:
-            if not self.element_en_liste(self.preferences, fourmi, True):
-                is_init = True
-                while(is_init or self.preferences.count((fourmi, dessert)) > 0):
-                    is_init = False
+            if not self.element_en_liste(self.preferences, fourmi, 1):
+                dessert = choice(listeDesserts)
+                while self.preferences.count((fourmi, dessert)):
                     dessert = choice(listeDesserts)
-
                 self.preferences.append((fourmi, dessert))
-
+        
         for dessert in listeDesserts:
-            if not self.element_en_liste(self.preferences, dessert, False):
-                is_init = True
-                while(is_init or self.preferences.count((fourmi, dessert)) > 0):
-                    is_init = False
+            if not self.element_en_liste(self.preferences, dessert, 0):
+                fourmi = choice(listeFourmis)
+                while self.preferences.count((fourmi, dessert)):
                     fourmi = choice(listeFourmis)
-
                 self.preferences.append((fourmi, dessert))
-
-    """
-        Retourne les fourmis qui n'apparaissent qu'une fois dans
-         le tableau des préférences.
-    """
-    def fourmi_une_fois(self):
-        # Nombre d'occurrences de chaque fourmi
-        nb_occurrences = []
-        for f in self.fourmis:
-            nb_occurrences.append(0)
+    
+    def desserts_correspondants(self, fourmi):
+        """
+            Retourne la liste des desserts correspondants à une fourmi donnée.
+        """
+        return [p[1] for p in self.preferences if p[0] == fourmi]
+    
+    def get_nb_occurrences(self):
+        """
+            Retourne une liste contenant la liste des occurrences
+             de chaque fourmi.
+        """
+        nb_occurrences = [0 for f in self.fourmis]
         
         for p in self.preferences:
             nb_occurrences[self.fourmis.index(p[0])] += 1
         
-        fourmis_1 = []
-        for i in range(len(nb_occurrences)):
-            if nb_occurrences[i] == 1:
-                fourmis_1.append(self.fourmis[i])
-        
-        return fourmis_1
-        
-
+        return nb_occurrences
+    
+    def occurrences_de_fourmi(self, nb_fois):
+        """
+            Retourne une liste des fourmis qui n'apparaissent que nb_fois dans
+             le tableau des préférences.
+        """ 
+        return [self.fourmis[i] for i, j in enumerate(self.get_nb_occurrences()) if j == nb_fois]
+    
+    def desserts_de_fourmi(self, fourmi):
+        """
+            Retourne la liste des desserts non encore attribués, correspondant
+             à une fourmi donnée.
+        """
+        return [d for f, d in self.preferences if f == fourmi]
+    
+    def supprimer_preferences_de(self, fourmi, dessert):
+        """
+            Supprime tous les couples contenant la fourmi ou le dessert
+             fourni en paramètre.
+        """
+        for preference in self.preferences:
+            if(preference[0] == fourmi or preference[1] == dessert):
+                self.preferences.remove(preference)
+    
     def resoudre(self):
+        solution = []
+        
         print self.preferences
-        for fourmi in self.fourmi_une_fois():
-            print "La fourmi", fourmi[0], "n'apparait qu'une fois"
+        
+        for i in range(1, 1 + max(self.get_nb_occurrences())):
+            print "- Traitement des fourmis de niveau", i
+            for fourmi in self.occurrences_de_fourmi(i):
+                print "-- Traitement de la fourmi", fourmi[0]
+                for dessert in self.desserts_de_fourmi(fourmi):
+                    print "--- La", fourmi[0], "aime le", dessert[0]
+                    solution.append((fourmi, dessert))
+                    self.supprimer_preferences_de(fourmi, dessert)
+        
+        print solution
 
 # algorithme
 listeFourmis = [("Fourmi A", "fourmi-a.png"), ("Fourmi B", "fourmi-b.png"), ("Fourmi C", "fourmi-c.png")]
 listeDesserts = [("Flan", "flan.png"), ("Banane", "banane.png"), ("Gâteau au chocolat", "gateau.png")]
 
-mod = ModelisationCouplage(listeFourmis, listeDesserts, 50)
+mod = ModelisationCouplage(listeFourmis, listeDesserts, 3)
 
 mod.resoudre()
 
