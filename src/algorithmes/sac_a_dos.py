637243e1da448c306910033381c493412ae2f148
jvoisin <pouicpouicpouic@gmail.com>
2011-10-06 21:46:39 +0300

    More pythonic style and less typos

--- src/algorithmes/sac_a_dos.py
+++ src/algorithmes/sac_a_dos.py
@@ -4,72 +4,85 @@
 
 class Object(object):
     '''
-        the class Object allow you to create object with a name, a value, and a weight
+        The class Object allow you to create object with a name, a value, and a weight
     '''
 
-    def __init__(self,name,weight,value):
+    def __init__(self, name, weight, value):
         self.name = name
         self.weight = weight
         self.value = value
-        self.rapport = self.value/self.weight
+        self.rapport = self.value / self.weight
 
 
 class Case(object):
     '''
-        the class Case allow you to create case which are used to solve the Bag problem
+        The class Case allow you to create case which are used to solve the Bag problem
     '''
     def __init__(self):
         self.value = 0
         self.weight = 0
-        self.object = []
+        self.content = []
 
-    def copy(self,case):
+    def copy(self, case):
+        '''
+            Copy case in the current object
+        '''
         self.value = case.value
         self.weight = case.weight
-        self.object = case.object[:]
+        self.content = case.content[:]
     
     def addO(self,obj):
+        '''
+            add an object to the case
+        '''
         self.value += obj.value
         self.weight += obj.weight
-        self.object.append(obj)
+        self.content.append(obj)
     
     def show(self):
-        print('value')
-        print(self.value)
-        print('weight')
-        print(self.weight)
-        print('Object')
-        for i in self.object:
-            print(i.name)
+        print('value: %s' % self.value)
+        print('weight: %s' % self.weight)
+        print('Object: ' + ''.join([str(i.name) for i in self.content])
+
 
 class Bag(object):
     '''
-        the class bag allow you to create bag who are defined by a list of object and a weight
+        The class bag allow you to create bags
+        who are defined by a list of object and a weight
     '''
-    def __init__(self,Lobject,weight):
+    def __init__(self, Lobject, weight):
         self.Lobject = Lobject[:]
         self.numObj = len(Lobject)
         self.weight = weight
-        self.tab = [[ Case() for j in range(self.weight+1)] for i in range(self.numObj+1)]
+        self.tab = [[ Case() for j in range(self.weight + 1)] for i in range(self.numObj + 1)]
 
-    def solve(self):# this algorithme is ussing a table of Case[number of object + 1][weight of the bag +1]
-        for i in xrange(self.numObj+1):
-            for j in xrange(self.weight+1):
-                if j!=0 and i!=0:   #the first line stand for 0 object in the bag and the first colonne stand for a current weight of 0
-                    if j-self.Lobject[i-1].weight >=0: # if the weight of the current object is lower or equall to the current weight
-                        w = self.Lobject[i-1].weight# w is the weight of the current object
-                        if self.tab[i-1][j].value > self.Lobject[i-1].value + self.tab[i-1][j-w].value: # if the value of the previous object at the current weight, is greater than the value of the current object plus the value of the previous object at the current weight minus the weight of the current object
-                            self.tab[i][j].copy(self.tab[i-1][j])# then you just take the case of the previous object at the current weight
+    def solve(self):
+        '''
+            This algorithm is using a table of Case[number of object + 1][weight of the bag +1]
+        '''
+        for i in xrange(self.numObj + 1):
+            for j in xrange(self.weight + 1):
+                if j != 0 and i != 0:
+                    #the first line stand for 0 object in the bag and the first column
+                    #stand for a current weight of 0
+                    if j >= self.Lobject[i-1].weight:
+                        w = self.Lobject[i-1].weight  # w is the weight of the current object
+                        if self.tab[i-1][j].value > self.Lobject[i-1].value + self.tab[i-1][j-w].value:
+                            #if the value of the previous object at the current weight,
+                            #is greater than the value of the current object plus the value of the previous
+                            #one at the current weight minus the weight of the current object
+                            self.tab[i][j].copy(self.tab[i-1][j])
+                            # then you just take the case of the previous object at the current weight
                         else:
-                            self.tab[i][j].copy(self.tab[i-1][j-w])#otherwise you take the case of the previous object at the current weight minus w 
-                            self.tab[i][j].addO(self.Lobject[i-1])# and you add the current object
-                    else:# if if the weight of the current object his higher than the current weight then just copy the case of the previous object at the same weight
+                            self.tab[i][j].copy(self.tab[i-1][j-w])
+                            #otherwise you take the case of the previous object at the current weight minus w 
+                            self.tab[i][j].addO(self.Lobject[i-1])
+                            #and add the current object
+                    else:
+                        # if the weight of the current object his higher than the current weight
+                        #then just copy the case of the previous object at the same weight
                         self.tab[i][j].copy(self.tab[i-1][j])
-        print('objet')
-        for i in self.Lobject:
-            print i.name
-        print('weight of the bag')
-        print(self.weight)
+                        
+        print('object: ' + ''.join([i.name for i in self.Lobject]))
+        print('weight of the bag: %s' % self.weight)
         self.tab[self.numObj][self.weight].show()  
-
-
