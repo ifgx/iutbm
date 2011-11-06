42b44af5096844664adf2f413039a7b48aab4859
jvoisin <julien.voisin@dustri.org>
2011-11-06 21:43:27 +0100

    Fix tabulation error

--- src/algorithmes/algo.py
+++ src/algorithmes/algo.py
@@ -1,44 +1,45 @@
 import pygame
 
 class Algo:
-	'''
-		Abstract class which represent
-		an algorithm
-	'''
-	def __init__(self, display):
-		self.display = display
-		self.font = pygame.font.Font(None, 17)
+    '''
+        Abstract class which represent
+        an algorithm
+    '''
+    def __init__(self, display):
+        self.display = display
+        self.font = pygame.font.Font(None, 17)
 
-	def _solve(self):
-		'''
-			Solve the problem
-		'''
-		raise NotImplementedError
+    def _solve(self, ):
+        '''
+            Solve the problem
+        '''
+        raise NotImplementedError
 
-	def _explain(self):
-		'''
-			Display a short explaination text
-		'''
-		raise NotImplementedError
-	
-	def _reset(self):
-		'''
-			Reset a problem
-		'''
-		pass
-	
-	def _draw(self):
-		'''
-			Draw the current state of the problem
-		'''
-		raise NotImplementedError
-		
-	def _update(self, (x, y)):
-		'''
-			Update the state of the problem:
-			this method is called only on mouse click.
-			(FIXME : call this method every frames ?)
-			x and y represent the position of
-			the mouse click.
-		'''
-		raise NotImplementedError
+    def _explain(self):
+        '''
+            Display a short explaination text
+        '''
+        text = self.font.render(self.text, True, (255, 0, 0))
+        self.display.blit(text, (10, 10))
+
+    def _reset(self):
+        '''
+            Reset a problem
+        '''
+        pass
+
+    def _draw(self):
+        '''
+            Draw the current state of the problem
+        '''
+        raise NotImplementedError
+
+    def _update(self, (x, y)):
+        '''
+            Update the state of the problem:
+            this method is called only on mouse click.
+            (FIXME : call this method every frames ?)
+            x and y represent the position of
+            the mouse click.
+        '''
+        raise NotImplementedError
