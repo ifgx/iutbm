4d05d364904d010438729288a4d5cc78d030585d
jvoisin <julien.voisin@dustri.org>
2011-11-06 19:48:49 +0100

    Even more corrections

--- src/algorithmes/plus_court.py
+++ src/algorithmes/plus_court.py
@@ -1,47 +1,56 @@
+'''
+    Shortest path problem
+'''
+
 import random
 import pygame
 import algo
 
-from math import pi,cos,sin, sqrt
-from pygame.locals import *
+from pygame.locals import * #FIXME : Why ?
+import math
+
 
 class Graphe(algo.Algo):
-    """
+    '''
         A class that represent a connex graph
-    """
+    '''
     def __init__(self,window):
         algo.Algo.__init__(self, window)
         self.numSommet=7
         minimum = 3 #FIXME : maximum of what ? : need rename
         maximum = 20 #FIXME : same as above
+        self.final = self.weight = 0 #FIXME : rename
+        self.next = [] #same as above : next is a KEYWORD !
+        self.selected = []
 
-        self.center_x = self.display.get_width()/2
-        self.center_y = self.display.get_height()/2
+        self.center_x = self.display.get_width() / 2
+        self.center_y = self.display.get_height() / 2
         if self.display.get_width() > self.display.get_height():
-            self.rayon = self.display.get_height()/3
+            self.rayon = self.display.get_height() / 3
         else:
-            self.rayon = self.display.get_width()/3
+            self.rayon = self.display.get_width() / 3
 
         self.LSommet = []
         # creation de la liste de point et de leur position
-        tmp = 2 * pi / (self.numSommet - 2)
+        tmp = 2 * math.pi / (self.numSommet - 2)
         for i in xrange(self.numSommet):
-            x = self.center_x + cos(tmp*i)*self.rayon
-            y = self.center_y + sin(tmp*i)*self.rayon
+            x = self.center_x + math.cos(tmp * i) * self.rayon
+            y = self.center_y + math.sin(tmp * i) * self.rayon
             self.LSommet.append(Sommet(i, i, x, y, 'algorithmes/ville.jpg'))
         self.start = self.LSommet[0]
+        self.current = self.start
         self.start.rect.center = (self.center_x / 3, self.center_y / 3)
         self.LSommet[self.start.indice].score = 0
 
         self.end = self.LSommet[self.numSommet - 1]
-        self.end.rect.center=(self.center_x* 5 / 3,self.center_y * 5 / 3)#FIXME
+        self.end.rect.center=(self.center_x* 5 / 3,self.center_y * 5 / 3)#FIXME hardcode result of 5/3
 
         # creation de la matrice d'adjacence
         self.Matrix = [[float('inf') for i in xrange(self.numSommet)] for i in xrange(self.numSommet)]
 
         # creation d'un graphe connexe aleatoire
         a = [i for i in xrange(self.numSommet - 1)]
-        now = 0
+        now = 0 #FIXME : rename
         for i in xrange(self.numSommet - 1):
             weight = random.randint(minimum, maximum)
             if i < self.numSommet - 2:
@@ -64,14 +73,11 @@ class Graphe(algo.Algo):
                             self.Matrix[j][i] = weight
                     self.Matrix[i][j] = self.Matrix[j][i]
 
-        # the two next line are to make sure that there is not  a direct connenxion between the start and the end
+        # make sure that the is no direct connection between
+        # the start point, and the end point.
         self.Matrix[self.start.indice][self.end.indice]= float('inf')
         self.Matrix[self.end.indice][self.start.indice]= float('inf')
 
-        self.final = self.weight = 0
-        self.current = self.start
-        self.next = []
-        self.selected = []
         self.nextSommet(self.start)
 
     def _draw(self): #FIXME: more comments !
@@ -90,7 +96,7 @@ class Graphe(algo.Algo):
             pygame.draw.rect(self.display,(255,0,0),self.start.rect)
             pygame.draw.rect(self.display,(0,255,0),self.end.rect)
 
-        if self.final == 1:
+        if self.final == 1: #FIXME : Why 1 ?
             if (self.weight > self.end.score):
                 text = self.font.render("You did not found the optimal path",True , (0,255,0))
             else:
@@ -132,7 +138,7 @@ class Graphe(algo.Algo):
     def coordonner(self, sommet2, sommet1): #FIXME : wtf ?
         x = sommet2.centery- sommet1.centery
         y = sommet1.centerx - sommet2.centerx
-        k = 15 / (sqrt(x*x+y*y))
+        k = 15 / (math.sqrt(x*x+y*y))
         x = k*x + (sommet2.centerx + sommet1.centerx ) / 2
         y = k*y + (sommet2.centery + sommet1.centery ) / 2
         return (x,y)
@@ -174,12 +180,13 @@ class Sommet(object):
         self.score = float('inf')
         self.visited = False
         self.previous = None  # the previous Sommet
-        self.name = name
+        self.name = name #FIXME name = indice ?
         self.image = pygame.image.load(image).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
-def visiterS(x):
+
+def visiterS(x): #FIXME : wtf ? Inline this !
     if x.visited == False:
         return x.score
     else:
