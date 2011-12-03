01892adadc1649b6419cd94b88ed770a08231e14
czambel2 <zmbl.soft@gmail.com>
2011-12-03 17:31:45 +0100

    Avancé l'algorithme du couplage

--- src/algorithmes/couplage.py
+++ src/algorithmes/couplage.py
@@ -1,58 +1,155 @@
 #-*-coding:utf8-*-
-from random import choice, randrange
+from random import choice, randrange, shuffle
 
 import pygame
+import algo
 
-class Couplage:
+class Couplage(algo.Algo):
     def __init__(self, display):
-        self.display = display
+        algo.Algo.__init__(self, display)
         
         # Génération aléatoire des fourmis et des desserts
         nbFourmis = randrange(3, 6)
         nbDesserts = randrange(3, 6)
         
-        theDir = "algorithmes/couplage-data/"
+        print "Sélection de", nbFourmis, "fourmis et", nbDesserts, "desserts."
         
-        allFourmis = [ ("Fourmi rouge", "fourmi-rouge.png"), ("Fourmi verte", "fourmi-verte.png"), ("Fourmi bleue", "fourmi-bleue.png"), ("Fourmi jaune", "fourmi-jaune.png"), ("Fourmi orange", "fourmi-orange.png") ]
+        theDir = "ui/pix/couplage/"
+        
+        allFourmis = [
+            ("George",  "client-blanc.png" ),
+            ("Ulrich",  "client-bleu.png"  ),
+            ("Michael", "client-orange.png"),
+            ("John",    "client-vert.png"  ),
+            ("Patrick", "client-violet.png")]
+        
+        allDesserts = [
+            ("Caper pizza",       "pizza-capres.png"    ),
+            ("Margherita pizza",  "pizza-margherita.png"),
+            ("Onions pizza",      "pizza-oignons.png"   ),
+            ("Pepperoni pizza",   "pizza-pepperoni.png" ),
+            ("Mushroom pizza",    "pizza-reine.png"     )]
+        
+        shuffle(allFourmis)
+        shuffle(allDesserts)
         
         self.imgFourmis = [pygame.image.load(theDir + img).convert_alpha() for _, img in allFourmis]
-        
-        allDesserts = [ ("Mars", "mars.png"), ("Pomme", "pomme.png"), ("Flan", "flan.png"), ("Twix", "twix.png"), ("Cerise", "cerise.png") ]
-        
         self.imgDesserts = [pygame.image.load(theDir + img).convert_alpha() for _, img in allDesserts]
         
         self.fourmis = [allFourmis[i] for i in range(nbFourmis)]
         self.desserts = [allDesserts[i] for i in range(nbDesserts)]
+        
+        self.fourmiSelectionnee = (-1, None)
+        
+        self.erreur = ""
+        
+        self.model = ModelisationCouplage(self.fourmis, self.desserts, 5)
+    
+    def _update(self, (x, y),button):
+        # reset error
+        self.erreur = ""
+        
+        if button == 1:
+            # if we have selected a fourmi, then create a line from the fourmi to the dessert
+            dessertSelectionne = False
+            for i, dessert in enumerate(self.desserts):
+                # check whether the user clicked on the dessert
+                if x >= 128 + 384 and x <= 128 + 384 + 32: # x coordinate is good
+                    y_coord = 192 + (64 * i)
+                    if y >= y_coord and y <= y_coord + 32: # y coordinate is good
+                        # a dessert is clicked
+                        if self.fourmiSelectionnee != (-1, None):
+                            fourmi = self.fourmiSelectionnee[1]
+                            dessertSelectionne = True
+                            print "Cliqué sur le dessert", i, "(", dessert[0], ")"
+                            # check if the fourmi or dessert is not already involved
+                            if not self.model.fourmi_dans(fourmi, self.model.proposition):
+                                if not self.model.dessert_dans(dessert, self.model.proposition):
+                                    print "Validé la combinaison (", fourmi[0], ",", dessert[0], ")"
+                                    self.model.proposition.append((fourmi, dessert))
+                                else:
+                                    print "Le dessert", dessert[0], "est déjà attribué"
+                                    self.erreur = "The pizza " + dessert[0] + " is already assigned"
+                            else:
+                                print "La fourmi", fourmi[0], "est déjà attribuée"
+                                self.erreur = "The customer " + fourmi[0] + " has already a pizza"
+                            
+                        else:
+                            self.erreur = "Please select a client first."
+            if not dessertSelectionne:
+                # deselect the fourmi
+                self.fourmiSelectionnee = (-1, None)
+            
+            for i, fourmi in enumerate(self.fourmis):
+                # check whether the user clicked on the fourmi
+                if x >= 128 and x <= 128 + 32: # x coordinate is good
+                    y_coord = 192 + (64 * i)
+                    if y >= y_coord and y <= y_coord + 32: # y coordinate is good
+                        print "Cliqué sur la fourmi", i, "(", fourmi[0], ")"
+                        self.fourmiSelectionnee = (i, fourmi)
+        elif button == 3:
+            if len(self.model.proposition) != 0:
+                self.model.proposition.pop()
+        
     
     def _draw(self):
-        #titre = self.font.render("Bienvenue au Couplage", True, (0,255, 0) )
-        #titreRect = titre.get_rect()
-        #titreRect.top = 50
-        #titreRect.left = 50
-        #self.display.blit(text,textRect)
         
-        for i, fourmi in enumerate(self.fourmis): 
-            self.display.blit(self.imgFourmis[i], (50, 150 * i))
+        titre = self.font.render("Help the pizza deliveryman to deliver pizzas to his customers", True, (0, 255, 0) )
+        titreRect = titre.get_rect()
+        titreRect.top = 48
+        titreRect.centerx = self.display.get_rect().width / 2
+        self.display.blit(titre, titreRect)
+        
+        desc = self.font.render("Click on a customer to see which pizza they like. Then, click on the pizza you have chosen for this customer.", True, (255, 255, 255) )
+        descRect = desc.get_rect()
+        descRect.top = 64
+        descRect.centerx = self.display.get_rect().width / 2
+        self.display.blit(desc, descRect)
+        
+        if self.erreur != "":
+            err = self.font.render(self.erreur, True, (255, 0, 0) )
+            errRect = err.get_rect()
+            errRect.top = 96
+            errRect.centerx = self.display.get_rect().width / 2
+            self.display.blit(err, errRect)
+        
+        for couple in self.model.proposition:
+            fourmiI = self.fourmis.index(couple[0])
+            dessertI = self.desserts.index(couple[1])
+            pygame.draw.line(self.display, (255, 0, 0), (128 + 16, 192 + (64 * fourmiI) + 16), (128 + 384 + 16, 192 + (64 * dessertI) + 16), 3)
+        
+        for i, fourmi in enumerate(self.fourmis):
+            nom = self.font.render(fourmi[0], True, (255, 255, 255))
+            nomRect = nom.get_rect()
+            nomRect.centery = 192 + (64 * i) + 18
+            nomRect.right = 128 - 8
+            self.display.blit(nom, nomRect)
+            self.display.blit(self.imgFourmis[i], (128, 192 + (64 * i)))
         
         for i, dessert in enumerate(self.desserts):
-            self.display.blit(self.imgDesserts[i], (500, 150 * i))
-
-        if False:
-            [self.display.blit(i.image, i.rect) for i in self.LSommet]# then we draw the picture of the town
-
-            [pygame.draw.rect(self.display, (255, 0, 255), i.rect,2) for i in self.selected]# then we draw the weight of the link
-
-            pygame.draw.rect(self.display,(255,0,0),self.start.rect,2)
-            pygame.draw.rect(self.display,(0,255,0),self.end.rect,2)
-
-            if self.state_game == 1: 
-                if (self.weight > self.end.score):
-                    text = self.font.render("You did not found the optimal path",True , (0,255,0))
-                else:
-                    text = self.font.render("Congratulation : you found the optimal path wiht the lenght " + str(self.end.score).replace('[',' '),True , (0,255,0))
-                textRect = text.get_rect()
-                textRect.center =(self.center_x, 10)
-                self.display.blit(text,textRect)
+            nom = self.font.render(dessert[0], True, (255, 255, 255))
+            nomRect = nom.get_rect()
+            nomRect.centery = 192 + (64 * i) + 18
+            nomRect.left = 128 + 384 + 32 + 8
+            self.display.blit(nom, nomRect)
+            self.display.blit(self.imgDesserts[i], (128 + 384, 192 + (64 * i)))
+        
+        if self.fourmiSelectionnee != (-1, None):
+            # draw the preferences of the fourmi at the bottom of the screen
+            # fourmi pic
+            pic = self.imgFourmis[self.fourmiSelectionnee[0]]
+            self.display.blit(pic, (192, self.display.get_rect().height - 64))
+            
+            # fourmi name
+            nom = self.font.render(self.fourmiSelectionnee[1][0] + " likes:", True, (0, 255, 0) )
+            nomRect = nom.get_rect()
+            nomRect.bottom = self.display.get_rect().height - 40
+            nomRect.left = 192 + 32 + 8
+            self.display.blit(nom, nomRect)
+            
+            # fourmi preferences
+            for i, pref in enumerate(self.model.desserts_correspondants(self.fourmiSelectionnee[1])):
+                self.display.blit(self.imgDesserts[self.desserts.index(pref)], (192 + 32 + 128 + (i * 64), self.display.get_rect().height - 64))
 
 class ModelisationCouplage:
     """
@@ -65,10 +162,24 @@ class ModelisationCouplage:
             if elemt[1 - premier] == element:
                 return True
         return False
+    
+    def fourmi_dans(self, fourmi, liste):
+        """
+            Vérifie si une fourmi est déjà présente dans la liste fournie
+        """
+        return self.element_en_liste(liste, fourmi, 1)
+    
+    def dessert_dans(self, dessert, liste):
+        """
+            Vérifie si une fourmi est déjà présente dans la liste fournie
+        """
+        return self.element_en_liste(liste, dessert, 0)
 
     def __init__(self, listeFourmis, listeDesserts, nbCouples):
         # Génération d'une liste de préférences pour les fourmis
         self.preferences = []
+        self.solution = []
+        self.proposition = []
         
         self.fourmis = listeFourmis
         self.desserts = listeDesserts
