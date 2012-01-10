2deb546d8b70fb348f13fa2a1df2c56879b5b7eb
yunai39 <mamanaine@hotmail.fr>
2012-01-10 07:55:34 +0100

    Update src/ui/theme.py

--- src/ui/theme.py
+++ src/ui/theme.py
@@ -7,7 +7,7 @@ secondary_text_color = (0, 255, 0)
 main_background = 'ui/pix/menu_bg.jpg'
 
 # graph like stuffs
-cities = glob.glob('ui/pix/ville*')
+cities = glob.glob('ui/pix/Ville*')
 pizzas = glob.glob('ui/pix/couplage/pizza*')
 map_background = 'ui/pix/map.jpg'
 road_color = (50, 50, 50)
